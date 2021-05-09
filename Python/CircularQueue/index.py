39b0c8abc0ef70dd9f81bceb08a2ae2af46185d2
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2021-05-09 13:16:04 -0300

    feat: :sparkles: python circular queue

--- Python/CircularQueue/index.py
+++ Python/CircularQueue/index.py
@@ -8,28 +8,31 @@ class MyCircularQueue():
         self.head = self.tail = -1
 
     def enqueue(self, value):
+        # print("Enqueue, value:", value)
         if (self.next_tail_position() == self.head):
-            print("Circular queue is full")
+            print(f"Circular queue is full, value: {value} is not added")
         elif (self.head == -1):
             self.head = 0
             self.tail = 0
             self.queue[0] = value
+            self.printCQueue()
         else:
             self.tail = self.next_tail_position()
             self.queue[self.tail] = value
+            self.printCQueue()
+        
 
     def dequeue(self):
         if (self.head == -1):
             print("Circular queue is empty")
         elif (self.head == self.tail):
-            temp = self.queue[self.head]
             self.head = -1
             self.tail = -1
-            return temp
         else:
-            temp = self.queue[self.head]
             self.head = self.next_head_position()
-            return temp
+
+        self.printCQueue()
+        
 
     def next_tail_position(self):
         return (self.tail + 1) % self.k
@@ -37,21 +40,19 @@ class MyCircularQueue():
     def next_head_position(self):
         return (self.head + 1) % self.k
 
-    # def printCQueue(self):
-    #     if(self.head == -1):
-    #         print("No element in the circular queue")
-
-    #     elif (self.tail >= self.head):
-    #         for i in range(self.head, self.tail + 1):
-    #             print(self.queue[i], end=" ")
-    #         print()
-    #     else:
-    #         for i in range(self.head, self.k):
-    #             print(self.queue[i], end=" ")
-    #         for i in range(0, self.tail + 1):
-    #             print(self.queue[i], end=" ")
-    #         print()
-
+    def printCQueue(self):
+        if(self.head == -1):
+            print("No elemenst on circular queue")
+        elif (self.tail >= self.head):
+            for i in range(self.head, self.tail+ 1):
+                print(self.queue[i], end=" ")
+        else:
+            for i in range(self.head, self.k):
+                print(self.queue[i], end=" ")
+            for i in range(0, self.tail + 1):
+                print(self.queue[i], end=" ")
+        
+        print()
 
 
 
@@ -62,10 +63,21 @@ obj.enqueue(2)
 obj.enqueue(3)
 obj.enqueue(4)
 obj.enqueue(5)
-print("Initial queue")
-# obj.printCQueue()
+obj.enqueue(6)
 
 obj.dequeue()
 obj.dequeue()
-print("After removing an element from the queue")
-# obj.printCQueue()
+obj.dequeue()
+
+obj.enqueue(6)
+obj.enqueue(7)
+obj.enqueue(8)
+obj.enqueue(9)
+
+obj.dequeue()
+obj.dequeue()
+obj.dequeue()
+obj.dequeue()
+
+
+
