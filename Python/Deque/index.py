cc315c97ba4477b3eabdbcc32541f8b4f26f2d4c
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2021-05-10 16:49:41 -0300

    feat: :sparkles: Python deque complete

--- Python/Deque/index.py
+++ Python/Deque/index.py
@@ -1,1 +1,51 @@
 # Deque implementaion in python
+
+
+class Deque:
+    def __init__(self):
+        self.queue = []
+
+    def addFront(self, item):
+        self.queue.insert(0, item)
+        self.print()
+
+    def addNear(self, item):
+        self.queue.append(item)
+        self.print()
+
+
+    def removeFront(self):
+        self.queue = self.queue[1:]
+        self.print()
+
+    def removeNear(self):
+        self.queue = self.queue[:-1]
+        self.print()
+    
+    # def removeFront(self):
+    #     return self.items.pop(0)
+
+    # def removeRear(self):
+    #     return self.items.pop()
+
+
+    def print(self):
+        print(self.queue)
+
+deque = Deque()
+
+deque.addFront(1)
+deque.addFront(2)
+deque.addFront(3)
+
+deque.addNear(3)
+deque.addNear(2)
+deque.addNear(1)
+
+deque.removeNear()
+deque.removeNear()
+deque.removeNear()
+
+deque.removeFront()
+deque.addNear(1)
+deque.removeNear()
