7bfaa9d50ab0802cbfee28f9532e05fed99402df
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2021-07-29 08:52:31 -0300

    feat: :sparkles: Hash Table Linear Probing, __getitem__

--- Python/HashTable/Open Adressing/Linear Probing/index.py
+++ Python/HashTable/Open Adressing/Linear Probing/index.py
@@ -15,6 +15,7 @@ class hashTable:
     def __init__(self, size: int) -> None:
         self.table = [0]*size
         self.elementCount = 0
+        self.size = size
         # list(0 for _ in range(size))
 
     # method that checks if the hash table is full or not
@@ -42,23 +43,34 @@ class hashTable:
         if(self.table[self.position(key)] == 0):
             self.table[self.position(key)] = new_node
             self.elementCount += 1
+        elif(self.table[self.position(key)].key == key):
+            print(f"Update value of key: {key}1")
+            self.table[self.position(key)] = new_node
 
         # If already have a value on the position
         else:
             # The same thing as DO_WHILE but in python
             while(True):
-                print("colission")
+                print(
+                    f"key: {key} with hash of {self.position(key)} colission {colission_counter+1} times")
                 colission_counter += 1
-                if(self.table[self.position(key)+colission_counter] == 0):
-                    self.table[self.position(key)+colission_counter] = new_node
+                new_position = self.position(key)+colission_counter
+
+                if(new_position >= self.size):
+                    new_position -= self.size
+
+                if(colission_counter == self.size):
+                    print("Table is Full")
+                    break
+
+                if(self.table[new_position] == 0):
+                    self.table[new_position] = new_node
                     self.elementCount += 1
                     break
                 # If the key already exist and the value is new, update just the value not fill the hash table
-                elif(self.table[self.position(key)+colission_counter].key == key
-                     and self.table[self.position(key)+colission_counter].value != value):
-                    print(f"Update value of key: {key}")
-                    self.table[self.position(key)+colission_counter] = new_node
-                    self.elementCount += 1
+                elif(self.table[new_position].key == key):
+                    print(f"Update value of key: {key}2")
+                    self.table[new_position] = new_node
                     break
 
     def remove(self, key) -> None:
@@ -68,22 +80,44 @@ class hashTable:
                 break
 
     # method to display the hash table
-
     def __str__(self):
         string = ""
-        for i in self.table:
+        for index, i in enumerate(self.table):
             if(i != 0):
-                string += (f"[{i.key}] : {i.value}\n")
+                string += (f"{index} - [{i.key}] : {i.value}\n")
             else:
-                string += (f"[{i}]\n")
+                string += (f"{index} - [{i}]\n")
         return string
+
+    def __getitem__(self, key):
+        new_position = self.position(key)
+        colission_counter = 0
+        while(True):
+            print(colission_counter)
+            if(colission_counter == self.size):
+                print("not found")
+                return None
+
+            if(new_position + colission_counter >= self.size):
+                new_position -= self.size
+
+            new_position += colission_counter
+            if(self.table[new_position] == 0):
+                return None
+            elif(self.table[new_position].key == key):
+                return self.table[new_position].value
+            else:
+                colission_counter += 1
 
 
 # main function
-table1 = hashTable(10)
-table1.insert(4, 110)
-table1.insert("vitor", 10)
-table1.insert("vitor", 11)
-table1.insert("vitor", 12)
+hashT = hashTable(8)
 
-print(table1)
+hashT.insert(8, "110")
+hashT.insert(9, "110")
+hashT.insert("developer", [1, 2, 3, 4, 5])
+hashT.insert(9, "another string")
+hashT.insert("full-Stack", (1, 2, 3, 5))
+
+print(hashT["full-Stack"])
+print(hashT)
