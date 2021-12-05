40b6145601384e2b298b320c972b60066e8844a8
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2021-12-05 20:42:01 -0300

    feat: :sparkles: Start AVL Tree (Python)

---
+++ Python/AVL Tree/index.py
@@ -0,0 +1,32 @@
+# AVL tree implementation in Python
+
+
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.right = None
+        self.left = None
+        self.height = 1
+
+
+class AVLTree:
+    def insert() -> None:
+        pass
+
+    def delete() -> None:
+        pass
+
+    def leftRotate() -> None:
+        pass
+
+    def rigthRotate() -> None:
+        pass
+
+    def getBalance() -> None:
+        pass
+
+    def getHeight() -> None:
+        pass
+
+    def getMinValueNode() -> None:
+        pass
