19a683335977e6da5fd6ce56d882d74bfcd9075b
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2022-01-20 20:47:21 -0300

    feat: :sparkles: init Adjascency List representation in Python

---
+++ Python/Adjacency List/index.py
@@ -0,0 +1,30 @@
+# Adjascency List representation in Python
+
+
+class AdjNode:
+    def __init__(self, value):
+        pass
+
+class Graph:
+    def __init__(self, num):
+        pass
+
+    # Add edges
+    def add_edge(self, s, d):
+        pass
+
+    # Print the graph
+    def print_agraph(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
+    # Create graph and edges
+    # graph = Graph(V)
+    # graph.add_edge(0, 1)
+    # graph.add_edge(0, 2)
+    # graph.add_edge(0, 3)
+    # graph.add_edge(1, 2)
+
+    # graph.print_agraph()
