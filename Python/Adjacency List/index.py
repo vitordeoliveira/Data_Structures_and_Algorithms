da9be385f4bbd01b90aff5e4ac0c483cee682863
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2022-01-28 18:11:57 -0300

    feat: :sparkles: Adjascency List representation in Python

--- Python/Adjacency List/index.py
+++ Python/Adjacency List/index.py
@@ -3,28 +3,43 @@
 
 class AdjNode:
     def __init__(self, value):
-        pass
+        self.vertex = value
+        self.next = None
 
 class Graph:
     def __init__(self, num):
-        pass
+        self.V = num
+        self.graph = [None] * num
 
     # Add edges
-    def add_edge(self, s, d):
-        pass
+    def add_edge(self, beginning_with, to):
+        node = AdjNode(to)
+        node.next = self.graph[beginning_with]
+        self.graph[beginning_with] = node
+
+        # Make the graph undirected
+        node_b = AdjNode(beginning_with)
+        node_b.next = self.graph[to]
+        self.graph[to] = node_b
 
     # Print the graph
     def print_agraph(self):
-        pass
+        for i in range(self.V):
+            print(f"Vertex {i}:", end="")
+            tmp = self.graph[i]
+            while tmp is not None:
+                print(f" -> {tmp.vertex}", end="")
+                tmp = tmp.next
+            print()
 
 
 if __name__ == "__main__":
     pass
     # Create graph and edges
-    # graph = Graph(V)
-    # graph.add_edge(0, 1)
-    # graph.add_edge(0, 2)
-    # graph.add_edge(0, 3)
-    # graph.add_edge(1, 2)
+    graph = Graph(5)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 2)
+    graph.add_edge(0, 3)
+    graph.add_edge(1, 2)
 
-    # graph.print_agraph()
+    graph.print_agraph()
